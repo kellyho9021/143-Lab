86a403f15f2374359e7e434a1239bd9ce03e6a80
kellyho9021 <kellyho9021@yahoo.com>
2016-05-29 21:29:39 -0700

    Initialize project

---
+++ src/com/jimweller/cpuscheduler/FCFSSchedulingAlgorithm.java
@@ -0,0 +1,70 @@
+/** FCFSSchedulingAlgorithm.java
+ * 
+ * A first-come first-served scheduling algorithm.
+ *
+ * @author: Charles Zhu
+ * Spring 2016
+ *
+ */
+package com.jimweller.cpuscheduler;
+
+import java.util.*;
+
+public class FCFSSchedulingAlgorithm extends BaseSchedulingAlgorithm {
+
+	private PriorityQueue<Process> pQueue;
+    FCFSSchedulingAlgorithm(){
+        // Fill in this method
+        /*------------------------------------------------------------*/
+		pQueue = new PriorityQueue<Process>(8, new ProcArrivalComparator());
+
+
+        /*------------------------------------------------------------*/
+    }
+
+    /** Add the new job to the correct queue.*/
+    public void addJob(Process p){
+        // Fill in this method
+        /*------------------------------------------------------------*/
+		pQueue.add(p);
+
+
+        /*------------------------------------------------------------*/
+    }
+    
+    /** Returns true if the job was present and was removed. */
+    public boolean removeJob(Process p){
+        // Fill in this method
+        /*------------------------------------------------------------*/
+		return pQueue.remove(p);
+
+
+        /*------------------------------------------------------------*/
+    }
+
+    /** Transfer all the jobs in the queue of a SchedulingAlgorithm to another, such as
+    when switching to another algorithm in the GUI */
+    public void transferJobsTo(SchedulingAlgorithm otherAlg) {
+        Iterator<Process> i = pQueue.iterator();
+		while(i.hasNext())
+		{
+			otherAlg.addJob(i.next());
+			i.remove();
+		}
+    }
+
+    /** Returns the next process that should be run by the CPU, null if none available.*/
+    public Process getNextJob(long currentTime){
+        // Fill in this method
+        /*------------------------------------------------------------*/
+		return pQueue.peek();
+
+
+        /*------------------------------------------------------------*/
+    }
+
+    public String getName(){
+        return "First-Come First-Served";
+    }
+    
+}
